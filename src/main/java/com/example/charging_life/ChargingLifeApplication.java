b3a474b935a8e0ca541cf49c28d888af6ef12dd0
star-sil <kimse9811@naver.com>
2022-07-25 18:38:28 +0900

    init

---
+++ src/main/java/com/example/charging_life/ChargingLifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.charging_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargingLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargingLifeApplication.class, args);
+	}
+
+}
