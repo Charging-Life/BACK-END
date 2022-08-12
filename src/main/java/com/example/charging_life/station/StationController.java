85ea0f8945d06a28e154c87af6218d45e346d949
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-08-12 20:56:52 +0900

    update swagger

--- src/main/java/com/example/charging_life/station/StationController.java
+++ src/main/java/com/example/charging_life/station/StationController.java
@@ -3,7 +3,9 @@ package com.example.charging_life.station;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,9 +21,17 @@ public class StationController {
 
     private final StationService stationService;
 
+
+    @Operation(summary = "공공 api 받아오기", description = "성공하면 공공 api를 데이터베이스에 저장")
     @GetMapping("/station")
     public String getChargingStationApi() throws IOException {
         stationService.saveChargingStationData();
         return "success";
     }
+
+   /* @GetMapping("/station/staus")
+    public String getChargingStationApi(@RequestParam(name = "sdf") String param) throws IOException {
+
+    }
+    */
 }
--- src/main/java/com/example/charging_life/station/entity/ChargingStation.java
+++ src/main/java/com/example/charging_life/station/entity/ChargingStation.java
@@ -15,13 +15,9 @@ public class ChargingStation {
     @Id @GeneratedValue
     private Long id;
     private String statNm;
-<<<<<<< HEAD
-    private Integer statId;
-=======
     @OneToMany(mappedBy = "chargingStation")
     private List<Charger> charger;
     private String statId;
->>>>>>> e8ab9d9 (save api data at ChargingStation & Charger)
     private String address;
     private String location;
     private Double lat;
@@ -35,16 +31,10 @@ public class ChargingStation {
     private String limitDetail;
 
     @Builder
-<<<<<<< HEAD
-    public ChargingStation(String statNm, Integer statId, String address, String location,
-                           Double lat, Double lng, String useTime, Business business, Boolean parkingFree,
-                           String note, Boolean limitYn, String limitDetail) {
-=======
     public ChargingStation(String statNm, List<Charger> charger, String statId,
                            String address, String location, Double lat, Double lng,
                            String useTime, Business business, Boolean parkingFree, String note,
                            Boolean limitYn, String limitDetail) {
->>>>>>> e8ab9d9 (save api data at ChargingStation & Charger)
         this.statNm = statNm;
         this.charger = charger;
         this.statId = statId;
