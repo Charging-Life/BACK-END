01530a4e7147030772f651942d3550cb3af6b46b
star-sil <kimse9811@naver.com>
2022-10-08 18:28:27 +0900

    FIX 클래스네임 중복으로 인한 오류 수정
    다른 Dto 클래스와 이름 중복으로 인한 오류를 해결하기 위해 ChargingStationDto 클래스의 ChargerDto 클래스와 BusinessDto 클래스를 ChargingStationDto 클래스의 내부 클래스로 옮겼다.

--- src/main/java/com/example/charging_life/station/dto/ChargingStationDto.java
+++ src/main/java/com/example/charging_life/station/dto/ChargingStationDto.java
@@ -50,43 +50,43 @@ public class ChargingStationDto {
         this.city = chargingStation.getZcode().getCity();
 
     }
-}
 
-@Getter
-class ChargerDto {
-    private Integer chargerId;
-    private String chargerType;
-    private Integer outPut;
-    private Integer stat;
-    private Long statUpdDt;
-    private Long lastTsdt;
-    private Long lastTedt;
-    private Long nowTsdt;
+    @Getter
+    private class ChargerDto {
+        private Integer chargerId;
+        private String chargerType;
+        private Integer outPut;
+        private Integer stat;
+        private Long statUpdDt;
+        private Long lastTsdt;
+        private Long lastTedt;
+        private Long nowTsdt;
 
-    public ChargerDto(Charger charger) {
-        this.chargerId = charger.getChargerId();;
-        this.chargerType = charger.getChargerType();
-        this.outPut = charger.getOutput();
-        this.stat = charger.getStat();
-        this.statUpdDt = charger.getStatUpdDt();
-        this.lastTsdt = charger.getLastTsdt();
-        this.lastTedt = charger.getLastTedt();
-        this.nowTsdt = charger.getNowTsdt();
+        public ChargerDto(Charger charger) {
+            this.chargerId = charger.getChargerId();;
+            this.chargerType = charger.getChargerType();
+            this.outPut = charger.getOutput();
+            this.stat = charger.getStat();
+            this.statUpdDt = charger.getStatUpdDt();
+            this.lastTsdt = charger.getLastTsdt();
+            this.lastTedt = charger.getLastTedt();
+            this.nowTsdt = charger.getNowTsdt();
+        }
     }
-}
 
-@Getter
-class BusinessDto {
-    private String businessId;
-    private String business;
-    private String operator;
-    private String businessCall;
+    @Getter
+    private class BusinessDto {
+        private String businessId;
+        private String business;
+        private String operator;
+        private String businessCall;
 
-    public BusinessDto(Business business) {
-        this.businessId = business.getBusinessId();
-        this.business = business.getBusiness();
-        this.operator = business.getOperator();
-        this.businessCall = business.getBusinessCall();
+        public BusinessDto(Business business) {
+            this.businessId = business.getBusinessId();
+            this.business = business.getBusiness();
+            this.operator = business.getOperator();
+            this.businessCall = business.getBusinessCall();
+        }
     }
 }
 
