f67ccf13faf0d69fc78888ada0d9ec7e6625c19f
star-sil <kimse9811@naver.com>
2022-10-21 11:43:33 +0900

    FIX 회원이 아닌 경우 충정소 상세정보 확인 불가 오류 수정
    회원이 아닌경우 accessToken이 주어지지 않아 필수 인자 누락으로 오류가 발생했다. 따라서 accessToken이 주어질 경우, 주어지지 않을 경우로 나누어 코드를 수정했다.

--- src/main/java/com/example/charging_life/member/repo/JpaMemberDestinationRepo.java
+++ src/main/java/com/example/charging_life/member/repo/JpaMemberDestinationRepo.java
@@ -4,16 +4,13 @@ import com.example.charging_life.member.entity.Member;
 import com.example.charging_life.member.entity.MemberDestination;
 import com.example.charging_life.station.entity.ChargingStation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface JpaMemberDestinationRepo extends JpaRepository<MemberDestination,Long> {
     List<MemberDestination> findByChargingStation(ChargingStation chargingStation);
-    boolean existsByChargingStationAndMember(ChargingStation chargingStation, Member member);
+    boolean existsByIdAndMember(Long id, Member member);
     void deleteByChargingStationAndMember(ChargingStation chargingStation, Member member);
 }
